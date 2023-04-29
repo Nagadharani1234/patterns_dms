8cb3cceeddb8f5b6816579c80ed4c5d66b2cfce0
Nagadharani1234 <111799348+Nagadharani1234@users.noreply.github.com>
2023-04-29 15:52:43 -0500

    files added

---
+++ src/main/java/com/FellahClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.FellahClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.FellahClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository  extends CrudRepository<Trainer, Integer>{
+
+}
---
+++ src/test/java/com/FellahClasses/DMS/DogsManagementSystemmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FellahClasses.DMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogsManagementSystemmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
